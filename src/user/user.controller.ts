4aabb676ed59c4c924cbc480696433eed9b1df64
UT-03 <f20200458@pilani.bits-pilani.ac.in>
2022-09-11 17:43:59 +0530

    feat: add route to authenticate github user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 import { UserService } from './user.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('/api/user')
 export class UserController {
-    constructor(private readonly userService: UserService) { }
+    constructor(private readonly userService: UserService, private configService: ConfigService) { }
 
+    @Get('/github-oauth')
+    auth(@Req() req: Request, @Res() res: Response) {
+        this.userService.GetGithubAuthToken(req.query.code.toString(), res);
+        console.log('this')
+
+    }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,51 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { HttpService } from '@nestjs/axios';
+import { Octokit } from 'octokit';
+import { Response } from 'express';
 
 @Injectable()
 export class UserService {
+    constructor(
+        private configService: ConfigService,
+        private readonly httpService: HttpService
+    ) { }
 
+    async GetGithubAuthToken(code: string, res: Response) {
+        const gitHubClientId = this.configService.get<string>('GITHUB_OAUTH_CLIENT_ID');
+        const gitHubClientSecret = this.configService.get<string>('GITHUB_OAUTH_CLIENT_SECRET');
+
+        this.httpService.get(`https://github.com/login/oauth/access_token?client_id=${gitHubClientId}&client_secret=${gitHubClientSecret}&code=${code}`)
+            .subscribe(async (response) => {
+                const authToken = response.data.split('&')[0].split('=')[1];
+
+                const octokit = new Octokit({
+                    auth: authToken
+                });
+
+                const { login } = (await octokit.rest.users.getAuthenticated()).data;
+
+                const repoName = `Dummy-Repo-${Math.random()}`;
+
+                await octokit.rest.repos.createForAuthenticatedUser({
+                    name: repoName
+                });
+
+                const fileContent = "VGhpcyBmaWxlIGlzIHB1c2hlZCB0byBnaXRodWIgdXNpbmcgb2N0b2tpdC4uLg==";
+
+                await octokit.request(`PUT /repos/${login}/${repoName}/contents/file.txt`, {
+                    owner: `${login}`,
+                    repo: `${repoName}`,
+                    path: 'file.txt',
+                    message: 'new file by octocat',
+                    committer: {
+                        name: 'Octocat bot',
+                        email: 'octocat@github.com'
+                    },
+                    content: `${fileContent}`
+                });
+
+                res.redirect(`${this.configService.get<string>('FRONTEND_URL')}/home`);
+            });
+    }
 }
